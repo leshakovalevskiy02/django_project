aeedcfcfffa37f14ae39f74a7322628c813b9335
leshakovalevsliy02 <lesha_kovalevskiy_gt_02@mail.ru>
2025-04-16 16:23:18 +0300

    My first urls and related views

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,3 +1,8 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return HttpResponse("Страница приложения women")
+
+def categories(request):
+    return HttpResponse("Категория <h1> blabla </h1>")
