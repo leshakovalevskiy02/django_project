12c321464314b98c22fd95fe36cdff7457bf6c6f
leshakovalevsliy02 <lesha_kovalevskiy_gt_02@mail.ru>
2025-04-17 00:11:44 +0300

    int and slug converters, custom converters, re_path

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -1,8 +1,12 @@
-from django.urls import path
-from . import views
+from django.urls import path, re_path, register_converter
+from . import views, converters
+
+register_converter(converters.YearConverter, "year4")
 
 
 urlpatterns = [
-    path('', views.index),  # http://127.0.0.1:8000/cats/
-    path('cats/', views.categories),  # http://127.0.0.1:8000/cats/
+    path('', views.index),  # http://127.0.0.1:8000
+    path('cats/<int:cat_id>/', views.categories),  # http://127.0.0.1:8000/cats/3/
+    path('cats/<slug:cat_slug>/', views.categories_by_slug),  # http://127.0.0.1:8000/cats/jenny/
+    path("archive/<year4:year>/", views.archive)  # http://127.0.0.1:8000/archive/2025/
 ]
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 def index(request):
     return HttpResponse("Страница приложения women")
 
-def categories(request):
-    return HttpResponse("Категория <h1> blabla </h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"Категория {cat_id} <h1> blabla </h1>")
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"{cat_slug} категория")
+
+def archive(request, year):
+    return HttpResponse(f"Архив по годам - {year}")
