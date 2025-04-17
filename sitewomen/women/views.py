6ae71f2653827e41e12a10cbd00903a617231779
leshakovalevsliy02 <lesha_kovalevskiy_gt_02@mail.ru>
2025-04-17 22:54:28 +0300

    get params, handlers

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, Http404
 from django.shortcuts import render
 
 def index(request):
@@ -8,7 +8,15 @@ def categories(request, cat_id):
     return HttpResponse(f"Категория {cat_id} <h1> blabla </h1>")
 
 def categories_by_slug(request, cat_slug):
+    if request.GET:
+        print(request.GET)
     return HttpResponse(f"{cat_slug} категория")
 
 def archive(request, year):
+    if year > 2023:
+        raise Http404()
+
     return HttpResponse(f"Архив по годам - {year}")
+
+def page_not_found(request, exception):
+    return HttpResponseNotFound("<h1>Страница не найдена</h1>")
