e1fdd6dc46e447ad85a7edcca1888b81cc302d44
leshakovalevsliy02 <lesha_kovalevskiy_gt_02@mail.ru>
2025-04-20 22:46:41 +0300

    template if and for tags

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,33 +1,22 @@
 from django.http import HttpResponse, HttpResponseNotFound, Http404, HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.template.loader import render_to_string
 from django.urls import reverse
-from django.template.defaultfilters import add
 from women.templatetags.custom_filters import add_email_to_string
 
 menu = ["О сайте", "Добавить статью", "Обратная связь", "Войти"]
 
-class MyClass:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-
-    def show_coords(self):
-        return f"Координата точки - {self.x, self.y}"
+data_db = [
+    {'id': 1, 'title': 'Анджелина Джоли', 'content': 'Биография Анджелины Джоли', 'is_published': True},
+    {'id': 2, 'title': 'Марго Робби', 'content': 'Биография Марго Робби', 'is_published': False},
+    {'id': 3, 'title': 'Джулия Робертс', 'content': 'Биография Джулия Робертс', 'is_published': True},
+]
 
 
 def index(request):
-    # t = render_to_string("women/index.html")
-    # return HttpResponse(t)
     data = {
         "title": "Главная страница",
         "menu": menu,
-        "float": 10.8,
-        "tpl": (1, 2, "abc", True),
-        "set": {1, 2, 3, 4, 5},
-        "dict": {"key_1": "value_1", "key_2": "value_2"},
-        "obj": MyClass(10, 20),
-        "value": add(30.7, 50)
+        "posts": [post for post in data_db if post["is_published"]]
     }
     return render(request, "women/index.html", context=data)
 
