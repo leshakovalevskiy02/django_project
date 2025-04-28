e9c5466ab0f5a4d0b7632ad1ff8f7eefc357025b
leshakovalevsliy02 <lesha_kovalevskiy_gt_02@mail.ru>
2025-04-28 14:22:44 +0300

    Adding a one-to-many relationship

--- sitewomen/women/models.py
+++ sitewomen/women/models.py
@@ -19,6 +19,7 @@ class Women(models.Model):
     time_create = models.DateTimeField(auto_now_add=True)
     time_update = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=Status.PUBLISHED, choices=Status.choices)
+    cat = models.ForeignKey("Category", on_delete=models.PROTECT)
 
     published = PublishedManager()
     objects = models.Manager()
@@ -34,3 +35,11 @@ class Women(models.Model):
 
     def get_absolute_url(self):
         return reverse("post", args=(self.slug, ))
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True, db_index=True)
+
+    def __str__(self):
+        return self.name
--- sitewomen/women/templatetags/women_tags.py
+++ sitewomen/women/templatetags/women_tags.py
@@ -1,5 +1,6 @@
 from django import template
 import women.views as views
+from women.models import Category
 
 
 register = template.Library()
@@ -12,9 +13,5 @@ def get_categories():
 
 @register.inclusion_tag("women/list_categories.html")
 def show_categories(cat_selected=0):
-    cats = views.cats_db
+    cats = Category.objects.all()
     return {"cats": cats, "cat_selected": cat_selected}
-
-@register.simple_tag()
-def get_default_title(title="Без заголовка"):
-    return title
