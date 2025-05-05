4911affea42c413274d3c243d9c59c92a1c4e9c9
leshakovalevsliy02 <lesha_kovalevskiy_gt_02@mail.ru>
2025-05-05 16:01:03 +0300

    Adding fields for searching and filtering, creating custom admin panel filters

--- sitewomen/women/admin.py
+++ sitewomen/women/admin.py
@@ -1,9 +1,48 @@
+from datetime import date
 from django.contrib import admin, messages
 from django.db.models import Prefetch
-
 from .models import Women, Category
 from django.db.models.functions import Length
 from django.db.models import Count, Q
+
+
+class ContentFilter(admin.SimpleListFilter):
+    title = "Сортировка по статьям"
+    parameter_name = 'content'
+
+    def lookups(self, request, model_admin):
+        return [
+            ("short", "Короткие статьи"),
+            ("middle", "Средние статьи"),
+            ("long", "Большие статьи")
+        ]
+
+    def queryset(self, request, queryset):
+        queryset = queryset.annotate(length=Length("content"))
+        if self.value() == "short":
+            return queryset.filter(length__lt=800)
+        if self.value() == "middle":
+            return queryset.filter(length__range=(800, 1999))
+        if self.value() == "long":
+            return queryset.filter(length__gte=2000)
+
+
+class MarriedFilter(admin.SimpleListFilter):
+    parameter_name = "status"
+    title = "Статус женщин"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("single", "Незамужняя"),
+            ("married", "Замужем"),
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value() == "single":
+            return queryset.filter(husband=None)
+        if self.value() == "married":
+            return queryset.filter(~Q(husband=None))
+
 
 @admin.register(Women)
 class WomenAdmin(admin.ModelAdmin):
@@ -13,6 +52,8 @@ class WomenAdmin(admin.ModelAdmin):
     list_editable = ("is_published", )
     list_per_page = 5
     actions = ("set_published", "set_draft")
+    search_fields = ("title__startswith", "cat__name")
+    list_filter = ("cat__name", "is_published", MarriedFilter, ContentFilter)
 
     @admin.display(description="Краткое описание", ordering=Length("content"))
     def brief_info(self, women: Women):
