8541d751c98d0f1c4d430b14cdb1dd37e68b8050
leshakovalevsliy02 <lesha_kovalevskiy_gt_02@mail.ru>
2025-05-23 00:57:41 +0300

    Adding pagination to a site

--- sitewomen/women/utils.py
+++ sitewomen/women/utils.py
@@ -10,6 +10,7 @@ menu = [{'title': "О сайте", 'url_name': 'about'},
 class DataMixin(ContextMixin):
     extra_context = {}
     title = None
+    paginate_by = 4
 
     def __init__(self):
         if self.title is not None:
@@ -20,4 +21,8 @@ class DataMixin(ContextMixin):
         context["menu"] = menu
         if "cat_selected" not in context:
             context["cat_selected"] = None
+        if "paginator" in context:
+            context["elided_page_range"] = context["paginator"].get_elided_page_range(
+                        context["page_obj"].number, on_each_side=2, on_ends=1)
+        print(context)
         return context
--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -1,5 +1,6 @@
+from django.core.paginator import Paginator
 from django.http import HttpResponse, HttpResponseNotFound
-from django.shortcuts import redirect, get_object_or_404
+from django.shortcuts import redirect, get_object_or_404, render
 from django.urls import reverse, reverse_lazy
 from .models import Women, TagPost
 from .forms import AddPostForm, ContactForm, UploadFilesForm
@@ -75,7 +76,12 @@ class Contact(DataMixin, FormView):
 
 
 def login(request):
-    return HttpResponse("Регистрация")
+    contact_list = Women.published.all()
+    paginator = Paginator(contact_list, 3)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
+    return render(request, "women/paginator.html", context=
+            DataMixin().get_context_data(title="Авторизация", page_obj=page_obj))
 
 
 class ShowCategory(DataMixin, ListView):
