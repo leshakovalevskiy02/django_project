25d96900c4f8bd1613b1f32ad3de6d92b90fd205
leshakovalevsliy02 <lesha_kovalevskiy_gt_02@mail.ru>
2025-05-29 18:31:32 +0300

    Implementation of password change using PasswordChangeView and PasswordChangeDoneView classes and standard PasswordChangeForm form

--- sitewomen/users/urls.py
+++ sitewomen/users/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, reverse_lazy
 from users import views
-from django.contrib.auth.views import LogoutView
+from django.contrib.auth.views import LogoutView, PasswordChangeView, PasswordChangeDoneView
+from .forms import UserPasswordChangeForm
 
 app_name = 'users'
 
@@ -9,5 +10,13 @@ urlpatterns = [
     path("logout/", LogoutView.as_view(), name="logout"),  # users:logout
     path("registration/", views.RegistrationUser.as_view(), name="registration"),
     path("registration_done/", views.registration_done, name="registration_done"),
-    path("profile/", views.UserProfile.as_view(), name="profile")
+    path("profile/", views.UserProfile.as_view(), name="profile"),
+    path("password_change/", PasswordChangeView.as_view(
+                                template_name="users/password_change.html",
+                                success_url=reverse_lazy("users:password_change_done"),
+                                form_class=UserPasswordChangeForm),
+                                name="password_change"),
+    path("password_change/done/", PasswordChangeDoneView.as_view(
+                                template_name="users/password_change_done.html",
+    ), name="password_change_done")
 ]
