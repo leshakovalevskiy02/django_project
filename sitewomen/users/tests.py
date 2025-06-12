d0c75dd913a383e48e55ce7254806265a5f53227
leshakovalevsliy02 <lesha_kovalevskiy_gt_02@mail.ru>
2025-06-12 13:57:08 +0300

    Adding tests to get the main page of the site, the correctness of displaying the site content obtained from the database, and the registration form

--- sitewomen/users/tests.py
+++ sitewomen/users/tests.py
@@ -1,3 +1,48 @@
+from http import HTTPStatus
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class RegisterUserTestCase(TestCase):
+    fixtures = ["users_permissions.json"]
+
+    def setUp(self):
+        self.data = {
+            'username': 'user_1',
+            'email': 'user1@sitewomen.ru',
+            'first_name': 'Sergey',
+            'last_name': 'Balakirev',
+            'password1': '12345678Aa',
+            'password2': '12345678Aa',
+        }
+
+    def test_form_registration_get(self):
+        path = reverse("users:registration")
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, "users/registration.html")
+
+    def test_form_registration_success(self):
+        user_model = get_user_model()
+        path = reverse('users:registration')
+        response = self.client.post(path, self.data)
+        self.assertRedirects(response, reverse('users:registration_done'), HTTPStatus.FOUND, HTTPStatus.OK)
+        self.assertTrue(user_model.objects.filter(username=self.data['username']).exists())
+
+    def test_user_registration_password_error(self):
+        self.data["password2"] = '12345678A'
+        path = reverse("users:registration")
+        response = self.client.post(path, self.data)
+        form = response.context['form']
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertIn("Введенные пароли не совпадают.", form.errors["password2"])
+
+    def test_user_registration_username_error(self):
+        path = reverse("users:registration")
+        user_model = get_user_model()
+        user_model.objects.create(username=self.data["username"])
+        response = self.client.post(path, self.data)
+        form = response.context['form']
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertIn("Пользователь с таким именем уже существует.", form.errors["username"])
--- sitewomen/women/tests.py
+++ sitewomen/women/tests.py
@@ -1,3 +1,58 @@
 from django.test import TestCase
+from django.urls import reverse
+from http import HTTPStatus
+from .models import Women
+from .utils import DataMixin
 
-# Create your tests here.
+
+class GetPagesTestCase(TestCase):
+    fixtures = ['women_women.json', 'women_category.json', 'women_husbands.json',
+                'women_tagspost.json', "users_authors.json", "users_groups.json"]
+
+    def setUp(self):
+        "Инициализация перед выполнением каждого теста"
+
+    def test_mainpage(self):
+        path = reverse("home")
+        response = self.client.get(path)
+        self.assertEqual(response.status_code, HTTPStatus.OK)
+        self.assertTemplateUsed(response, "women/index.html")
+        self.assertEqual(response.context_data["title"], "Главная страница")
+
+    def test_addpage_redirect(self):
+        path = reverse("add_page")
+        path_redirect = reverse("users:login")
+        response = self.client.get(path)
+        self.assertRedirects(response, f"{path_redirect}?next={path}",
+                             HTTPStatus.FOUND, HTTPStatus.OK)
+
+    def test_data_mainpage(self):
+        posts = Women.published.select_related("cat").select_related("author")
+        path =  reverse("home")
+        response = self.client.get(path)
+        self.assertQuerySetEqual(posts[:DataMixin.paginate_by], response.context_data["posts"])
+
+    def test_paginate_pages(self):
+        path = reverse("home")
+        response = self.client.get(path)
+        num_pages = response.context_data["paginator"].num_pages
+        posts = Women.published.select_related("cat").select_related("author")
+
+        page = 1
+        paginate_by = DataMixin.paginate_by
+
+        while page <= num_pages:
+            response = self.client.get(f"{path}?page={page}")
+            self.assertQuerySetEqual(response.context_data["posts"],
+                                 posts[(page - 1) * paginate_by: page * paginate_by])
+            page += 1
+
+    def test_content_post(self):
+        posts = Women.published.all()
+        for post in posts:
+            path = reverse("post", args=(post.slug, ))
+            response = self.client.get(path)
+            self.assertEqual(post.content, response.context_data["post"].content)
+
+    def tearDown(self):
+        "Действия после выполнения каждого теста"
